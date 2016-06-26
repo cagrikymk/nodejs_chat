f1d5ddc0d6b9d68c4c86f9d14521b6e2858c2dc1
Çağrı <mehmet.kaymak@ug.bilkent.edu.tr>
2016-06-26 05:52:14 +0300

    asa

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var session = require('express-session');
+
+var sess;
+
+app.use(session({secret: 'my-super-chat'}));
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  if(sess == null)
+    sess = req.session;
+  if(sess.nickname != null) {
+    res.sendFile(__dirname + '/index.html');
+    console.log(sess.nickname);
+  }
+
+  else
+    res.sendFile(__dirname + '/index.html');
+
+  console.log(sess.nickname == null);
 });
 
 io.on('connection', function(socket){
@@ -11,10 +26,12 @@ io.on('connection', function(socket){
   });
 
   socket.on('nick-thing', function(msg) {
+    sess.nickname = msg;
+    console.log(sess.nickname);
   	socket.emit('nick-thing', msg);
   });
 });
 
-http.listen(3000, function(){
+http.listen(80, function(){
   console.log('listening on *:3000');
 });
