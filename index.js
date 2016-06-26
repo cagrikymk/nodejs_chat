6373ce162d602aad508df353fba14294daba3682
Çağrı <mehmet.kaymak@ug.bilkent.edu.tr>
2016-06-26 06:01:21 +0300

    rollback

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var session = require('express-session');
-
-var sess;
-
-app.use(session({secret: 'my-super-chat'}));
 app.get('/', function(req, res){
-  if(sess == null)
-    sess = req.session;
-  if(sess.nickname != null) {
-    res.sendFile(__dirname + '/index.html');
-    console.log(sess.nickname);
-  }
-
-  else
-    res.sendFile(__dirname + '/index.html');
-
-  console.log(sess.nickname == null);
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
@@ -26,12 +11,10 @@ io.on('connection', function(socket){
   });
 
   socket.on('nick-thing', function(msg) {
-    sess.nickname = msg;
-    console.log(sess.nickname);
   	socket.emit('nick-thing', msg);
   });
 });
 
-http.listen(80, function(){
+http.listen(3000, function(){
   console.log('listening on *:3000');
 });
